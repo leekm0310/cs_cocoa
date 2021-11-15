f0191cc9bf41eef8b158f50f913babf08d425ab5
Kyungmi Lee <leekm310@gmail.com>
2021-11-15 22:23:33 +0900

    한글 시계 구현중 메인 메소드 정리함...

--- src/day11/Clock.java
+++ src/day11/Clock.java
@@ -9,24 +9,26 @@ import java.util.Map;
 
 public class Clock {
     public static void main(String[] args) {
+        getTime();
+    }
+
+    public static void getTime() {
+
+        Map hm = new HashMap();
 
         while(true) {
             LocalTime time = LocalTime.now();
             int hour = time.get(ChronoField.CLOCK_HOUR_OF_AMPM);
             int minute = time.getMinute();
-
-            Map hm = new HashMap();
             hm.put("hour", hour);
             hm.put("minute", minute);
             change(hm);
-
             try {
-                Thread.sleep(10000);
+                Thread.sleep(30000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-
     }
 
     public static void change(Map map){
@@ -38,20 +40,29 @@ public class Clock {
         String[] min1 = {"","","이","삼","사","오"};
         String[] min2 = {"","일","이","삼","사","오","육","칠","팔","구","십"};
 
-
         int h1 = hour / 10;
         int h2 = hour % 10;
 
+        if ( h1 == 1 && h2 == 0){
+            String hour_first = hours1[h1];
+            System.out.println(hour_first + "시");
+        } else {
+            String hour_first = hours1[h1];
+            String hour_second = hours2[h2-1];
+            System.out.println(hour_first + hour_second + "시");
+        }
+
+
         int m1 = minute / 10;
         int m2 = minute % 10;
-        String hour_first = hours1[h1];
-        String hour_second = hours2[h2-1];
-        System.out.println(hour_first + hour_second + "시");
 
-        if (m1 != 0){
+        if (m1 != 0 && m2 != 0){
             String min_first = min1[m1] + "십";
             String min_second = min2[m2];
             System.out.println(min_first + min_second +"분");
+        } else if ( m1 == 1 && m2 == 0){
+            String min_first = "십";
+            System.out.println(min_first + "분");
         } else {
             String min_first = min1[m1];
             String min_second = min2[m2];
