b159850aa870f2a88629601c8567857ea0bfca24
Kyungmi Lee <leekm310@gmail.com>
2021-11-15 14:56:17 +0900

    한글시계 구현중
    시간을 어떻게 처리할까 싶어서
    처음에 로컬타임으로 썼다가 칼렌더를 썼다가
    다시 로컬타임으로 돌아온 코드.

--- src/day11/Clock.java
+++ src/day11/Clock.java
@@ -2,6 +2,8 @@ package day11;
 
 
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,10 +14,8 @@ public class Clock {
 
     public static Map getTime(){
         LocalTime time = LocalTime.now();
-        int hour = time.getHour();
+        int hour = time.get(ChronoField.CLOCK_HOUR_OF_AMPM);
         int minute = time.getMinute();
-        LocalTime tim = LocalTime.of(hour,minute);
-        System.out.println("현재시간 " + tim);
 
         Map hm = new HashMap();
         hm.put("hour", hour);
@@ -25,22 +25,32 @@ public class Clock {
     }
 
     public static void change(Map map){
+
         int hour = (int) map.get("hour");
         int minute = (int) map.get("minute");
-        String[] hours = {"한", "두", "세", "네", "다섯", "여섯", "일곱", "여덟", "아홉",
-                "열", "열한", "열두"};
+        String[] hours1 = {"","열"};
+        String[] hours2 = {"한", "두", "세", "네", "다섯", "여섯", "일곱", "여덟", "아홉"};
         String[] min1 = {"","","이","삼","사","오"};
         String[] min2 = {"일","이","삼","사","오","육","칠","팔","구","십"};
-        String h = hours[hour-1] + "시";
-        System.out.println(h);
+
+
+        int h1 = hour / 10;
+        int h2 = hour % 10;
 
         int m1 = minute / 10;
         int m2 = minute % 10;
+        String hour_first = hours1[h1];
+        String hour_second = hours2[h2-1];
+        System.out.println(hour_first + hour_second + "시");
 
-        String min_first = min1[m1];
-        String min_second = min2[m2 - 1];
-        System.out.println(min_first + min_second + "분");
-
-
+        if (m1 != 0){
+            String min_first = min1[m1] + "십";
+            String min_second = min2[m2 - 1];
+            System.out.println(min_first + min_second + "분");
+        } else {
+            String min_first = min1[m1];
+            String min_second = min2[m2 - 1];
+            System.out.println(min_first + min_second + "분");
+        }
     }
 }
