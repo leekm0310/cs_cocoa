3707e021b7f1044e70c645b531f88c4abc22dd9a
Kyungmi Lee <leekm310@gmail.com>
2021-11-16 00:13:07 +0900

    shell 만들기 일단 대충 만들고 나서 수정 작업 할 예정. 디렉토리 변경 하는거 짜야함

--- src/day11/Shell.java
+++ src/day11/Shell.java
@@ -1,9 +1,15 @@
 package day11;
 
-import java.io.File;
+import java.io.*;
 
 public class Shell {
+
+
     public static void main(String[] args) {
+       Copy();
+    }
+
+    public static void Read() {
         String pathName = "C:\\Users\\user\\Downloads\\practice";
         File file = new File(pathName);
         System.out.println(pathName + " is exist?? " + file.exists());
@@ -11,8 +17,30 @@ public class Shell {
         String[] fileNames = file.list();
         for (int i = 0; i < fileNames.length; i++){
             System.out.println(fileNames[i]);
-            }
         }
+    }
 
+    public static void Copy() {
+        String pathName = "C:\\Users\\user\\Downloads\\practice\\test.txt";
+        String copyName = "C:\\Users\\user\\Downloads\\practice\\test2.txt";
+        File file = new File(pathName);
+        File copyFile = new File(copyName);
+
+        try {
+            FileInputStream fis = new FileInputStream(file);
+            FileOutputStream fos = new FileOutputStream(copyFile);
+
+            int fileByte = 0;
+            while((fileByte = fis.read()) != -1){
+                fos.write(fileByte);
+            }
+            fis.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
 
