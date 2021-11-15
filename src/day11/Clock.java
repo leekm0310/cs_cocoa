625ce5db7a0364675d958d2d15b5dddc8b1f95b1
Kyungmi Lee <leekm310@gmail.com>
2021-11-15 12:01:35 +0900

    한글 시계 구현 change 메소드 코드 정리

--- src/day11/Clock.java
+++ src/day11/Clock.java
@@ -29,33 +29,18 @@ public class Clock {
         int minute = (int) map.get("minute");
         String[] hours = {"한", "두", "세", "네", "다섯", "여섯", "일곱", "여덟", "아홉",
                 "열", "열한", "열두"};
-        String[] min1 = {"이","삼","사","오"};
+        String[] min1 = {"","","이","삼","사","오"};
         String[] min2 = {"일","이","삼","사","오","육","칠","팔","구","십"};
         String h = hours[hour-1] + "시";
         System.out.println(h);
-        if (minute <= 10) {
-            String m = min2[minute-1];
-            System.out.println(m + "분");
-        } else if (10 < minute && minute < 20){
-            String m1 = "십";
-            String m2 = min2[minute-11];
-            System.out.println(m1 + m2 + "분");
-        } else if (20 < minute && minute < 30){
-            String m1 = min1[0] + "십";
-            String m2 = min2[minute-21];
-            System.out.println(m1 + m2 + "분");
-        } else if (30 < minute && minute < 40){
-            String m1 = min1[1] + "십";
-            String m2 = min2[minute-31];
-            System.out.println(m1 + m2 + "분");
-        } else if (40 < minute && minute < 50){
-            String m1 = min1[2] + "십";
-            String m2 = min2[minute-41];
-            System.out.println(m1 + m2 + "분");
-        } else {
-            String m1 = min1[3] + "십";
-            String m2 = min2[minute-51];
-            System.out.println(m1 + m2 + "분");
-        }
+
+        int m1 = minute / 10;
+        int m2 = minute % 10;
+
+        String min_first = min1[m1];
+        String min_second = min2[m2 - 1];
+        System.out.println(min_first + min_second + "분");
+
+
     }
 }
