373bd7d4d79ede74a7b68fc5da39590aeaff62eb
Kyungmi Lee <leekm310@gmail.com>
2021-11-25 17:00:23 +0900

    펜 기능 추가

--- src/day18/G3.java
+++ src/day18/G3.java
@@ -10,18 +10,20 @@ class G3 extends Frame {
     private int y2;
     private int dx1;
     private int dy1;
-    private boolean isdrawing = false;
+    private boolean drawing = false;
     private Image img = null;
     private Graphics gImg = null;
     private Button b1;
     private Button b2;
     private Button b3;
+    private Button b4;
     private int shape;
 
     static class Shape{
         static final int Line = 1;
         static final int Circle = 2;
         static final int Rectangle = 3;
+        static final int pen = 4;
     }
 
     public G3(String title){
@@ -39,9 +41,10 @@ class G3 extends Frame {
         addMouseMotionListener(new MouseAdapter() {
             @Override
             public void mouseDragged(MouseEvent e) {
-                if (isdrawing) {
+                if (drawing) {
                     dx1 = e.getX();
                     dy1 = e.getY();
+                    repaint();
                 }
             }
         });
@@ -50,9 +53,11 @@ class G3 extends Frame {
         b1 = new Button("line");
         b2 = new Button("circle");
         b3 = new Button("rectangle");
+        b4 = new Button("pen");
         b1.addActionListener(this::actionPerformed);
         b2.addActionListener(this::actionPerformed);
         b3.addActionListener(this::actionPerformed);
+        b4.addActionListener(this::actionPerformed);
 
         addComponent();
 
@@ -67,18 +72,25 @@ class G3 extends Frame {
         add(b1);
         add(b2);
         add(b3);
+        add(b4);
     }
 
 
     public void paint(Graphics g){
         if (shape == Shape.Line){
             g.drawLine(x1,y1,x2,y2);
+
         }
         if (shape == Shape.Circle) {
             g.drawOval(Math.min(x1,x2), Math.min(y1,y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
+
         }
         if (shape == Shape.Rectangle){
             g.drawRect(Math.min(x1,x2), Math.min(y1,y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
+
+        }
+        if (shape == Shape.pen){
+            g.fillOval(dx1,dy1,10,10);
         }
     }
 
@@ -103,14 +115,20 @@ class G3 extends Frame {
     public void actionPerformed(ActionEvent e){
         if (e.getSource() == b1){
             shape = Shape.Line;
+            drawing = false;
         }
         if (e.getSource() == b2){
             shape = Shape.Circle;
+            drawing = false;
         }
         if (e.getSource() == b3){
             shape = Shape.Rectangle;
+            drawing = false;
         }
-      // repaint();
+        if (e.getSource() == b4){
+            shape = Shape.pen;
+            drawing = true;
+        }
 
     }
 }
