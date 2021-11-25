7793539fab6552a62b89e5622ea921c76fb7e1a4
Kyungmi Lee <leekm310@gmail.com>
2021-11-25 17:53:08 +0900

    색바꾸기 구현 해야함

--- src/day18/Drawer.java
+++ src/day18/Drawer.java
@@ -17,7 +17,11 @@ class Drawer extends Frame {
     private Button b2;
     private Button b3;
     private Button b4;
+    private Button c1;
+    private Button c2;
+    private Button c3;
     private int shape;
+    private boolean isblue = false;
 
     static class Shape{
         static final int Line = 1;
@@ -49,16 +53,7 @@ class Drawer extends Frame {
             }
         });
 
-
-        b1 = new Button("line");
-        b2 = new Button("circle");
-        b3 = new Button("rectangle");
-        b4 = new Button("pen");
-        b1.addActionListener(this::actionPerformed);
-        b2.addActionListener(this::actionPerformed);
-        b3.addActionListener(this::actionPerformed);
-        b4.addActionListener(this::actionPerformed);
-
+        buttonMaker();
         addComponent();
 
         setVisible(true);
@@ -68,35 +63,53 @@ class Drawer extends Frame {
 
     }
 
+    void buttonMaker(){
+        b1 = new Button("line");
+        b2 = new Button("circle");
+        b3 = new Button("rectangle");
+        b4 = new Button("pen");
+        b1.addActionListener(this::actionPerformed);
+        b2.addActionListener(this::actionPerformed);
+        b3.addActionListener(this::actionPerformed);
+        b4.addActionListener(this::actionPerformed);
+        c1 = new Button("Blue");
+        c1.addActionListener(this::actionPerformed);
+
+    }
+
+
     void addComponent(){
         add(b1);
         add(b2);
         add(b3);
         add(b4);
+        add(c1);
     }
 
 
     public void paint(Graphics g){
         if (shape == Shape.Line){
             g.drawLine(x1,y1,x2,y2);
-
         }
         if (shape == Shape.Circle) {
             g.drawOval(Math.min(x1,x2), Math.min(y1,y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
-
         }
         if (shape == Shape.Rectangle){
             g.drawRect(Math.min(x1,x2), Math.min(y1,y2), Math.abs(x1 - x2), Math.abs(y1 - y2));
-
         }
         if (shape == Shape.pen){
             g.fillOval(dx1,dy1,10,10);
+        }
+        if (isblue = true){
+            g.setColor(Color.blue);
         }
     }
 
     public void update(Graphics g){
         paint(g);
     }
+
+
 
     private class Mouse extends MouseAdapter{
         @Override
@@ -111,6 +124,7 @@ class Drawer extends Frame {
             repaint();
         }
     }
+
 
     public void actionPerformed(ActionEvent e){
         if (e.getSource() == b1){
@@ -129,6 +143,13 @@ class Drawer extends Frame {
             shape = Shape.pen;
             drawing = true;
         }
+        if (e.getSource() == c1){
+            isblue = true;
+        }
 
     }
+
+
+
+
 }
